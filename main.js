a68cab7955165d59dfc6f645ec3c1cb6d7c004f3
Daniyar Orazov <orazov.video@gmail.com>
2023-02-03 15:31:51 +0100

    Added new style for range input

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+const rangeInput = document.querySelectorAll(".range-input input");
+const priceInput = document.querySelectorAll(".price-input input");
+const progress = document.querySelector(".slider .progress");
+
+let priceGap = 1000;
+
+rangeInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minVal = parseInt(rangeInput[0].value);
+        let maxVal = parseInt(rangeInput[1].value);
+
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+
+        
+    });
+});
+
+priceInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minVal = parseInt(priceInput[0].value);
+        let maxVal = parseInt(priceInput[1].value);
+
+        if ((maxVal - minVal >= priceGap) && maxVal <= 10000) {
+            if (e.target.className === "input-min") {
+                rangeInput[0].value = minVal;
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            } else {
+                rangeInput[1].value = maxVal;
+                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+            }
+        } 
+
+        
+    });
+});
+
+
 $(document).ready(() => {
     // showCards(htmlBlock, data)
     filter()
@@ -19,8 +67,10 @@ function filter() {
 
     $("#filterPriceButton").on('click', () => {
         $(listBlockElems).removeClass('d-none');
-        const inputMin = $("#filterPriceInputMin").val();
-        const inputMax = $("#filterPriceInputMax").val();
+        let minVal = parseInt(rangeInput[0].value);
+        let maxVal = parseInt(rangeInput[1].value);
+        const inputMin = minVal;
+        const inputMax = maxVal;
         prices.map((el, key) => {
             // 1000 - 5000 .... 3450
             if (!(+inputMin < el && +inputMax > el)) {
