c16559a66e1a08fa2082d842b8e97dee6a1d5874
Daniyar Orazov <orazov.video@gmail.com>
2023-02-03 23:45:39 +0100

    Refactored code, improve functionality and logic filter

--- main.js
+++ main.js
@@ -1,84 +1,93 @@
-const rangeInput = document.querySelectorAll(".range-input input");
-const priceInput = document.querySelectorAll(".price-input input");
-const progress = document.querySelector(".slider .progress");
-
-let priceGap = 1000;
-
-rangeInput.forEach(input => {
-    input.addEventListener("input", e => {
-        let minVal = parseInt(rangeInput[0].value);
-        let maxVal = parseInt(rangeInput[1].value);
-
-        if (maxVal - minVal < priceGap) {
-            if (e.target.className === "range-min") {
-                rangeInput[0].value = maxVal - priceGap;
-            } else {
-                rangeInput[1].value = minVal + priceGap;
-            }
-        } else {
-            priceInput[0].value = minVal;
-            priceInput[1].value = maxVal;
-            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-        }
-
-        
-    });
+$(document).ready(() => {
+    const mainCardsBlockClass = "prodSlot"; // main cards block element from list
+    const priceClass = "salePrice"; // class of price elements
+    const priceAttrValue = "data-bs-priceFilter"; // name of attr with a value with prices from price elements
+    const buttonIdHandler = "filterPriceButton"; // id button handler for filter
+    filterFunc(mainCardsBlockClass, priceClass, priceAttrValue, buttonIdHandler, rangePriceInputConf);
 });
 
-priceInput.forEach(input => {
-    input.addEventListener("input", e => {
-        let minVal = parseInt(priceInput[0].value);
-        let maxVal = parseInt(priceInput[1].value);
+// Settings of range slider GUI
+function rangePriceInputConf() {
+    const rangeInput = document.querySelectorAll(".range-input input");
+    const priceInput = document.querySelectorAll(".price-input input");
+    const progress = document.querySelector(".slider .progress");
+    const inputMinRange = document.querySelector("#inputMinRange");
+    const maxValue = inputMinRange.getAttribute('max');
+    let priceGap = 1000;
 
-        if ((maxVal - minVal >= priceGap) && maxVal <= 10000) {
-            if (e.target.className === "input-min") {
-                rangeInput[0].value = minVal;
-                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+    
+
+    rangeInput.forEach(input => {
+        input.addEventListener("input", e => {
+            let minVal = parseInt(rangeInput[0].value);
+            let maxVal = parseInt(rangeInput[1].value);
+
+            if (maxVal - minVal < priceGap) {
+                if (e.target.className === "range-min") {
+                    rangeInput[0].value = maxVal - priceGap;
+                } else {
+                    rangeInput[1].value = minVal + priceGap;
+                }
             } else {
-                rangeInput[1].value = maxVal;
+                priceInput[0].value = minVal;
+                priceInput[1].value = maxVal;
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
                 progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
             }
-        } 
-
-        
+        });
     });
-});
 
+    priceInput.forEach(input => {
+        input.addEventListener("input", e => {
+            let minVal = parseInt(priceInput[0].value);
+            let maxVal = parseInt(priceInput[1].value);
 
-$(document).ready(() => {
-    // showCards(htmlBlock, data)
-    filter()
-});
+            if ((maxVal - minVal >= priceGap) && maxVal <= maxValue) {
+                if (e.target.className === "input-min") {
+                    rangeInput[0].value = minVal;
+                    progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+                } else {
+                    rangeInput[1].value = maxVal;
+                    progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                }
+            } 
+        });
+    });
 
-function showCards(htmlBlock, data) {
-
+    return priceInput;
 }
 
-function filter() {
-    const elems = document.getElementsByClassName('salePrice');
-    const listBlockElems = document.querySelectorAll('.prodSlot');
+// Function of filter elements from list
+function filterFunc(mainCardsBlockClass, priceClass, priceAttrValue, buttonIdHandler, priceInputFunc) {
+    const listBlockElems = document.querySelectorAll(`.${mainCardsBlockClass}`);
+    const priceInput = priceInputFunc();
+    const rangeInput = document.querySelectorAll(".range-input input");
     const prices = [];
-    const listBlock = document.querySelector('.list-products');
+    const maxValue = inputMinRange.getAttribute('max');
+    const progress = document.querySelector(".slider .progress");
 
-    $(".salePrice[data-bs-priceFilter]").val(function() {
-        prices.push($(this).attr("data-bs-priceFilter")) 
+    $(`.${priceClass}[${priceAttrValue}]`).val(function() {
+        prices.push($(this).attr(priceAttrValue)) 
     });
 
-    $("#filterPriceButton").on('click', () => {
+    $(`#${buttonIdHandler}`).on('click', () => {
         $(listBlockElems).removeClass('d-none');
-        let minVal = parseInt(rangeInput[0].value);
-        let maxVal = parseInt(rangeInput[1].value);
-        const inputMin = minVal;
-        const inputMax = maxVal;
+        let inputMin = parseInt(priceInput[0].value);
+        let inputMax = parseInt(priceInput[1].value);
+
+        if (inputMax > maxValue) {
+            rangeInput[1].value = maxValue;
+            priceInput[1].value = maxValue;
+            let percent = (rangeInput[1].value / maxValue) * 100;
+            progress.style.right = 100 - percent + "%";
+            progress.style.right = 100 - (priceInput[1].value / maxValue) * 100 + "%";
+        }
+
+
         prices.map((el, key) => {
-            // 1000 - 5000 .... 3450
-            if (!(+inputMin < el && +inputMax > el)) {
+            if (!(+inputMin <= el && +inputMax >= el)) {
                 $(listBlockElems[key]).addClass('d-none');
             }
         })
-        
     });
-
-    
 }
